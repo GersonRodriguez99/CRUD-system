8bd4c1d319ba3c35157e16b2b3d6e7f50e893e73
GersonRodriguez99 <gersonrs99@gmail.com>
2020-11-05 19:56:46 -0600

    added new message

---
+++ CRUD/database/migrations/2020_11_06_013755_create_client_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('apellido');
+            $table->string('dui');
+            $table->string('nit');
+            $table->string('direccion');
+            $table->integer('telefono');
+            $table->integer('celular');
+            $table->String('notas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client');
+    }
+}
